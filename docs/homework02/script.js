1eb3cf9c6f391be93a6815f9c17f4021342d58a9
wj-yoshida <yoshida@warpjapan.com>
2019-05-23 19:12:48 +0900

    update homework02

--- docs/homework02/script.js
+++ docs/homework02/script.js
@@ -33,7 +33,13 @@
     let small_hand;
     let big_hand;
     let hand_base;
+    let pillar;
 
+    let dw_st_time = 0;
+    let up_st_time = 0;
+    let duration = 40;
+    let POS = { mnt_hand: 0, small_hand: 0, big_hand: 0, pillar: 0.8, pillar_pos: 0 };
+    let NXPOS =  { mnt_hand: 0, small_hand: 0, big_hand: 0, pillar: 0.8, pillar_pos: 0 };
     // constant variables
     const RENDERER_PARAM = {
         clearColor: 0x111111
@@ -78,30 +84,23 @@
         renderer = new THREE.WebGLRenderer();
         renderer.setClearColor(new THREE.Color(RENDERER_PARAM.clearColor));
         renderer.setSize(canvasWidth, canvasHeight);
+        renderer.shadowMap.enabled = true;
         targetDOM.appendChild(renderer.domElement);
         controls = new THREE.OrbitControls(camera, renderer.domElement);
 
-        // - グループを使う ---------------------------------------------------
-        // three.js のオブジェクトは、グループにひとまとめにすることができます。
-        // グループを使わないと実現できない挙動、というのも一部にはありますので、
-        // ここで使い方だけでもしっかり覚えておきましょう。
-        // 特に、グループに追加したことによって「回転や平行移動の概念が変わる」
-        // ということが非常に重要です。
-        // --------------------------------------------------------------------
-        // group @@@
         group = new THREE.Group();
 
         // material and geometory
         material = new THREE.MeshPhongMaterial(MATERIAL_PARAM);
         materialPoint = new THREE.PointsMaterial(MATERIAL_PARAM_POINT);
 
-
         //土台
-        geometry = new THREE.CylinderGeometry( 3.5, 3.5, 0.5, 30 );
+        geometry = new THREE.CylinderGeometry( 3.2, 3.2, 0.5, 30 );
         box = new THREE.Mesh( geometry, material );
         box.rotation.x = THREE.Math.degToRad(90);
         box.position.z = -0.5;
-        scene.add( box );
+        box.receiveShadow = true;
+        group.add( box );
 
         // torus
         geometry = new THREE.TorusGeometry(0.9, 0.02, 32, 32);
@@ -109,15 +108,12 @@
         torus.position.x = 0.0;
         torus.position.z = 0.0;
         torus.scale.z = 0.04;
-        group.add(torus); // group に add する @@@
-
-        // group をシーンに加える @@@
-        scene.add(group);
+        group.add(torus);
 
         //文字盤
         font_loader = new THREE.FontLoader();
         font_loader.load( 'helvetiker_bold.typeface.json', function ( font ) {
-          let radius = 2.4;
+          let radius = 2.25;
           const center = 0;
           for (let i = 1; i < 13; i++) {
             let view_txt = "";
@@ -132,18 +128,18 @@
             txt[i].center();
             txt_materials = [
           	   //	new THREE.MeshBasicMaterial( { color: Math.random() * 0xffffff, overdraw: 0.5 } ),
-          		new THREE.MeshBasicMaterial( { color: 0x383838 } )
+          		new THREE.MeshPhongMaterial( { color: 0x383838 } )
           	];
           	textMesh[i] = new THREE.Mesh(txt[i], txt_materials);
             const t =  360/12 * i;
             textMesh[i].position.x = center + radius * Math.sin( t * (Math.PI / 180) ) ;
             textMesh[i].position.y = center + radius * Math.cos( t * (Math.PI / 180) ) ;
             textMesh[i].position.z = 0.05;
-          	scene.add(textMesh[i]);
+          	group.add(textMesh[i]);
           }
         } );
         font_loader.load( 'helvetiker_regular.typeface.json', function ( font ) {
-          let radius = 2.85;
+          let radius = 2.7;
           const center = 0;
           for (let i = 1; i < 13; i++) {
             let view_txt = 0;
@@ -158,7 +154,7 @@
             txt[i].center();
             txt_materials = [
           	   //	new THREE.MeshBasicMaterial( { color: Math.random() * 0xffffff, overdraw: 0.5 } ),
-          		new THREE.MeshBasicMaterial( { color: 0x383838 } )
+          		new THREE.MeshPhongMaterial( { color: 0x383838 } )
           	];
           	textMesh[i] = new THREE.Mesh(txt[i], txt_materials);
             const t =  360/12 * i;
@@ -166,7 +162,7 @@
             textMesh[i].position.y = center + radius * Math.cos( t * (Math.PI / 180) ) ;
             textMesh[i].position.z = 0.05;
             textMesh[i].rotation.z = THREE.Math.degToRad(-t);
-          	scene.add(textMesh[i]);
+          	group.add(textMesh[i]);
           }
         } );
 
@@ -190,48 +186,54 @@
 
         // メッシュを作成
         const tick_line = new THREE.Mesh(tick_line_geo, material);
-        scene.add(tick_line);
+        group.add(tick_line);
 
         //針の支柱
-        geometry = new THREE.CylinderGeometry( 0.1, 0.1, 0.5, 10 );
-        box = new THREE.Mesh( geometry, material );
-        box.rotation.x = THREE.Math.degToRad(90);
-        box.position.z = 0.5;
-        scene.add( box );
+        geometry = new THREE.CylinderGeometry( 0.1, 0.1, 0.8, 10 );
+        pillar = new THREE.Mesh( geometry, material );
+        pillar.rotation.x = THREE.Math.degToRad(90);
+        pillar.position.z = 0.5;
+        group.add( pillar );
 
         //秒針
         mnt_hand = new THREE.Group();
-        geometry = new THREE.BoxGeometry(0.05, 2.8, 0.05);
+        geometry = new THREE.BoxGeometry(0.06, 2.6, 0.02);
         box = new THREE.Mesh(geometry, material);
-        box.position.y = 1.4;
-        box.position.z = 0.7;
+        box.position.y = 1.2;
+        box.position.z = 0.3;
         mnt_hand.add(box);
-        geometry = new THREE.TorusGeometry(0.2, 0.02, 20, 20);
+        geometry = new THREE.TorusGeometry(0.2, 0.01, 20, 20);
         box = new THREE.Mesh(geometry, material);
         box.position.y = 2.7;
-        box.position.z = 0.7;
+        box.position.z = 0.3;
+        box.castShadow = true;
         mnt_hand.add(box);
-        scene.add(mnt_hand);
+        group.add(mnt_hand);
 
         //長針
         big_hand = new THREE.Group();
         geometry = new THREE.BoxGeometry(0.2, 1.5, 0.05);
         box = new THREE.Mesh(geometry, material);
         box.position.y = 0.75;
-        box.position.z = 0.6;
+        box.position.z = 0.2;
+        box.castShadow = true;
         big_hand.add(box);
-        scene.add(big_hand);
+        group.add(big_hand);
 
         //短針
         small_hand = new THREE.Group();
         geometry = new THREE.BoxGeometry(0.1, 2.0, 0.05);
         box = new THREE.Mesh(geometry, material);
         box.position.y = 1.0;
-        box.position.z = 0.5;
+        box.position.z = 0.1;
+        box.castShadow = true;
         small_hand.add(box);
-        scene.add(small_hand);
+        group.add(small_hand);
 
         set_time();
+
+        // group をシーンに加える @@@
+        scene.add(group);
 
         // lights
         directionalLight = new THREE.DirectionalLight(
@@ -241,12 +243,20 @@
         directionalLight.position.x = DIRECTIONAL_LIGHT_PARAM.x;
         directionalLight.position.y = DIRECTIONAL_LIGHT_PARAM.y;
         directionalLight.position.z = DIRECTIONAL_LIGHT_PARAM.z;
-        scene.add(directionalLight);
+        directionalLight.castShadow = true;
+        //scene.add(directionalLight);
         ambientLight = new THREE.AmbientLight(
             AMBIENT_LIGHT_PARAM.color,
             AMBIENT_LIGHT_PARAM.intensity
         );
-        scene.add(ambientLight);
+        //scene.add(ambientLight);
+
+        const spotlight = new THREE.SpotLight(0x555555, 1, 100, Math.PI / 3, 0.8);
+        spotlight.position.y = 5;
+        spotlight.position.z = 8;
+        spotlight.position.x = 5;
+        spotlight.castShadow = true;
+        scene.add(spotlight);
 
         // helper
         axesHelper = new THREE.AxesHelper(5.0);
@@ -257,12 +267,19 @@
             run = eve.key !== 'Escape';
             if(eve.key === ' '){
                 isDown = true;
+                up_st_time = 0;
             }
         }, false);
         window.addEventListener('keyup', (eve) => {
-            if(eve.key === ' '){
-                isDown = false;
-            }
+          if(eve.key === ' '){
+            isDown = false;
+            dw_st_time = 0;
+            NXPOS.mnt_hand = mnt_hand.position.z;
+            NXPOS.small_hand = small_hand.position.z;
+            NXPOS.big_hand = big_hand.position.z;
+            NXPOS.pillar = pillar.scale.y;
+            NXPOS.pillar_pos = pillar.position.z;
+          }
         }, false);
         window.addEventListener('resize', () => {
             renderer.setSize(window.innerWidth, window.innerHeight);
@@ -278,12 +295,30 @@
     function render(){
       if(run){requestAnimationFrame(render);}
       if(isDown === true){
-          // group に対して処理する @@@
-          group.rotation.z -= 0.02;
+        dw_st_time++;
+        if(dw_st_time > duration){
+          dw_st_time = duration;
+        }
+        mnt_hand.position.z = easing( dw_st_time, POS.mnt_hand, 1.8, duration);
+        small_hand.position.z = easing( dw_st_time, POS.small_hand, 1.2, duration+10);
+        big_hand.position.z = easing( dw_st_time, POS.big_hand, 0.6, duration+20);
+        pillar.scale.y = easing( dw_st_time, POS.pillar, 2, duration);
+        pillar.position.z = easing( dw_st_time, POS.pillar_pos, 1, duration);
+      }else {
+        up_st_time++;
+        if(up_st_time > duration){
+          up_st_time = duration;
+        }
+        mnt_hand.position.z = easing( up_st_time, NXPOS.mnt_hand, (POS.mnt_hand - NXPOS.mnt_hand), duration );
+        small_hand.position.z = easing( up_st_time, NXPOS.small_hand, (POS.small_hand - NXPOS.small_hand), duration);
+        big_hand.position.z = easing( up_st_time, NXPOS.big_hand, (POS.big_hand - NXPOS.big_hand), duration);
+        pillar.scale.y = easing( up_st_time, NXPOS.pillar, (POS.pillar - NXPOS.pillar), duration);
+        pillar.position.z = easing( up_st_time, NXPOS.pillar_pos, (POS.pillar_pos - NXPOS.pillar_pos), duration);
       }
       set_time();
       renderer.render(scene, camera);
     }
+
     function set_time(){
       let date = new Date();
       let h = date.getHours();
@@ -296,4 +331,10 @@
       small_hand.rotation.z = THREE.Math.degToRad(-(360/60*m) - (360/(60*60*6)*s));
       big_hand.rotation.z = THREE.Math.degToRad(-(360/12*h) - (360/(60*6)*m) - (360/(60*60*6)*s) );
     }
+
+    function easing(t, b, c, d) {
+      if ((t/=d/2) < 1) return c/2*t*t + b;
+    	return -c/2 * ((--t)*(t-2) - 1) + b;
+    }
+
 })();
